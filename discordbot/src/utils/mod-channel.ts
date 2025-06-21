4a3e84b48011320c4c785f18254f36f597d2b0db
wang-yuancheng <yuanchengntu@gmail.com>
2025-06-21 19:02:21 +0800

    Remove duplicated code

--- discordbot/src/utils/mod-channel.ts
+++
@@ -1,6 +0,0 @@
-import { getRedisClient } from "../clients/redis";
-
-export async function getModChannelID(guildID: string): Promise<string | null> {
-  const redis = getRedisClient();
-  return await redis.get(`modChannel:${guildID}`);
-}
