bf440e8ba87beecc0316bbb4f85b8d76a8224c6c
wang-yuancheng <yuanchengntu@gmail.com>
2025-07-10 06:32:53 +0800

    Update favicon

--- landing-page/src/app/layout.tsx
+++ landing-page/src/app/layout.tsx
@@ -6,8 +6,8 @@ import { twMerge } from "tailwind-merge";
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Light Saas Landing Page",
-  description: "Template created by Frontend Tribe",
+  title: "shibemod",
+  description: "Discord moderation bot",
 };
 
 export default function RootLayout({
